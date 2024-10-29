25df7c3dce8c0f9f6dffc8a7b7100aab5a8e9e4f
Brennan Reinhard <brennan.reinhard27@d155.org>
2024-10-29 07:53:34 -0500

    Initial Commit

---
+++ SciOlyUtil/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SciOlyUtil
+//
+//  Created by BRENNAN REINHARD on 10/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
